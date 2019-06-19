0a1aa80d4eea958dd257bea94bdfe556f82fc1ad
febanda01 <febanda91@gmail.com>
2019-06-19 17:40:29 -0500

    exercise-done

---
+++ Click-Drag-Scroll/notes.js
@@ -0,0 +1,7 @@
+//working with mousedown, mouseup, mouseleave, mousemove, mouseout
+
+//using classList.add and classList.remove to change the status of an element, in this case it was to detect whether the mouse was pressed down or not 
+
+//one way to tell exactly where you clicked on a page is to use e.pageX, will give you the exact x-coordinates 
+
+//capturing variables at the time of mousedown and then refrencing them when the user moves the mouse 
