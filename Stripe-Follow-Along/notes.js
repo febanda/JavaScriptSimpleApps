612729b71d69b6a88e50fc102f525e5be7158437
febanda01 <febanda91@gmail.com>
2019-06-18 23:17:09 -0500

    exercise-done

---
+++ Stripe-Follow-Along/notes.js
@@ -0,0 +1,11 @@
+//contents of the dropdown showing when hovering over a particular tab 
+
+//when you enter into a function the value of "this" changes however you can use an arrow function the value of this is inherinted from the parent function 
+
+//being able to use this.querySelector('.dropdown') to determine what dropdown class to display 
+
+//used .getBoundingClientRect() to retrieve the coordinates of the dropdowns. This gives us information about where on the page from the bottom to the left the top to the right and the width of the actual item/element 
+
+//using .setProperty to adjust the width and height of the white span element according to the dropdowns coords 
+
+//
